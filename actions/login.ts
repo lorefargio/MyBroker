9b72e36f81e49cdd57c78736ed9e785e813ea3d2
lorefargio <145431609+lorefargio@users.noreply.github.com>
2024-07-25 11:17:44 +0200

    LogIn e signOut

--- actions/login.ts
+++ actions/login.ts
@@ -1,12 +1,35 @@
 "use server"
 import * as z from "zod"
 import { LoginSchema } from "../schemas";
+import { signIn } from "../auth";
+import { DEFAULT_LOGIN_REDIRECT } from "../routes";
+import { AuthError } from "next-auth";
 
 export const login = async (values : z.infer<typeof LoginSchema>) => {
     const validateFields = LoginSchema.safeParse(values) ;
 
     if(!validateFields.success){
         return {error : "invalid fields!"} ;
+    }
+
+    const {email, password} = validateFields.data ;
+
+    try{
+        await signIn("credentials",{
+            email,
+            password,
+            redirectTo: DEFAULT_LOGIN_REDIRECT,
+        })
+    }catch(e){
+        if(e instanceof AuthError){
+            switch (e.type) {
+                case "CredentialsSignin":
+                    return {error: "Invalid Credential"} ;
+                default:
+                    return {error: "Somerthing went wrong!"}
+            }
+        }
+        throw e ;
     }
 
     return {success : "Email sent!"} ;
--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,33 @@
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "./schemas";
 import type { NextAuthConfig } from "next-auth"
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcryptjs"
  
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub,Google],
+  providers: [Credentials(
+    {
+      async authorize(credentials) {
+          const validateFields = LoginSchema.safeParse(credentials) ;
+
+          if(validateFields.success) {
+            const {email, password} = validateFields.data;
+
+            const user = await getUserByEmail(email) ;
+            if(!user || !user.password) return null ;
+
+            const passwordsMatch = await bcrypt.compare(
+              password,
+              user.password
+            );
+
+            if(passwordsMatch) return user ;
+          }
+          return null ;
+      }
+    }
+  )],
 } satisfies NextAuthConfig
