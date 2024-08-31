ad9394d0f842fd94293654d6e4f012c5c78fe092
Lorenzo Fargione <145431609+lorefargio@users.noreply.github.com>
2024-08-31 23:59:26 +0200

    Update layout.tsx

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -7,7 +7,7 @@ interface ProtectedLayoutProps {
     children: React.ReactNode;
 };
 
-export default async function ProtectedLayout({children} : ProtectedLayoutProps) {
+export default function ProtectedLayout({children} : ProtectedLayoutProps) {
     const session = await auth() ;
     return (
         
