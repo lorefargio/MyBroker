6d5b452bfb8a15259c9cc93a8292b268f2680669
lorefargio <145431609+lorefargio@users.noreply.github.com>
2024-09-02 11:51:46 +0200

    delete unused functions

--- src/api/finnhub/stock-api.ts
+++ src/api/finnhub/stock-api.ts
@@ -2,26 +2,6 @@
 
 const basePath = "https://finnhub.io/api/v1"
 
-export const searchSymbol = async (query : string) => {
-    const url =  `${basePath}/search?q=${query}&token=${process.env.FINNHUB_API_KEY}` ;
-
-    const response = await fetch(url) ;
-
-    if(!response.ok) throw new Error("Error occurred during searchSymbol API call "+response.status) ;
-
-    return await response.json() ;
-}
-
-export const fetchSymbolDetails = async (symbol : string) => {
-    const url = `${basePath}/stock/profile2?symbol=${symbol}&token=${process.env.FINNHUB_API_KEY}` ;
-    
-    const response = await fetch(url) ;
-
-    if(!response.ok) throw new Error("Error occurred during fetchSymbolDetails API call "+response.status) ;
-
-    return await response.json() ;
-}
-
 export const fetchQuote = async (symbol : string | undefined) => {
     const url = `${basePath}/quote?symbol=${symbol}&token=${process.env.FINNHUB_API_KEY}` ;
 
